37e88e53390a65270c646d4b1b535e452e29ab5a
Alllex202 <Alllex.202@gmail.com>
2021-05-06 13:04:18 +0500

    upd toolbar - changing color of the menu button

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
-  constructor() { }
+  menuIsOpen = false;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  menuOpened(): void {
+    this.menuIsOpen = true;
+  }
+
+  menuClosed(): void {
+    this.menuIsOpen = false;
   }
 
 }
